be44a84745830f7ffe5ea462b4fedc7787186da4
Vihaan0412 <vihaangandhi2102@gmail.com>
2026-06-09 17:45:44 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,17 @@
+a=int(input("Enter any number:  "))
+b=int(input("Enter any number:  "))
+c=int(input("Enter any number:  "))
+d=int(input("Enter any number:  "))
+
+hi= (a+b) * c/d
+print("hi:  ", hi)
+
+name= "Alex"
+age=0
+#'and' has higher precedence than 'or'
+if name == "Alex" or name == "John" and age >= 10:
+    print("Hello, Welcome")
+
+else:
+    print("Goodbye!!")
+
---
+++ a4.py
@@ -0,0 +1,7 @@
+a=int(input("Enter value 1:  "))
+b=int(input("Enter value 2:  "))
+c=int(input("Enter value 3:  "))
+
+avg = (a + b + c)/ 3
+
+print("avg:",avg)
